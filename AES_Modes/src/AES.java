68647080e4b98a647f038b63be1334cfa7161d21
ramcode <ramesh.thetakali@gmail.com>
2015-11-12 00:46:11 -0500

    Initial Commit

---
+++ AES_Modes/src/AES.java
@@ -0,0 +1,50 @@
+import java.security.MessageDigest;
+import java.util.Arrays;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES {
+  static String IV = "0000000000000000";
+  static String plaintext = "This sample test"; /*Note null padding*/
+  static String encryptionKey = "ShravyaRameshSri";
+  public static void main(String [] args) {
+    try {
+      
+      System.out.println("==Java==");
+      System.out.println("plain:   " + plaintext);
+
+      byte[] cipher = encrypt(plaintext, encryptionKey);
+
+      System.out.print("cipher:  ");
+      System.out.print(Util.toHEX(cipher));
+      System.out.println("");
+
+      String decrypted = decrypt(cipher, encryptionKey);
+
+      System.out.println("decrypt: " + decrypted);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } 
+  }
+
+  public static byte[] encrypt(String plainText, String encryptionKey) throws Exception {
+    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+    cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+    return cipher.doFinal(plainText.getBytes("UTF-8"));
+  }
+
+  public static String decrypt(byte[] cipherText, String encryptionKey) throws Exception{
+    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+    cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+    return new String(cipher.doFinal(cipherText),"UTF-8");
+  }
+}
